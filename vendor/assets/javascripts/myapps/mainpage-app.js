fbc74071cf9047c658ee8ee8c29d72561862434e
Takahiro Watanabe <takahiro.watanabe1220@gmail.com>
2014-08-15 21:00:25 +0900

    メイン画面の基礎を作成

--- vendor/assets/javascripts/myapps/mainpage-app.js
+++ vendor/assets/javascripts/myapps/mainpage-app.js
@@ -6,7 +6,27 @@ var mainView = myApp.addView('.view-main', {
 });
 
 var mainContentsCallbacks = myApp.onPageInit('main', function (page) {
-
+    var thisPanel = null;
+    $$('.closeFooterNotification').on('click', function () {        
+        $$('.footerNotification').hide();
+    });
+    $$(".answerPanels").on('click',function(){
+        myApp.popup('.popup-about');
+        thisPanel = $(this);
+        //
+    });
+    $$('#answerSubmit').on('click',function(){
+        myApp.closeModal('.popup-about');
+        thisPanel.css({
+            '-webkit-filter':'none',
+            'filter':'none'
+        });
+    });
+    $(function(){
+        $(".answerPanels").each(function(){
+            
+        });
+    });
 });
 
 // notification sample
@@ -16,3 +36,5 @@ var mainContentsCallbacks = myApp.onPageInit('main', function (page) {
 // 		message: 'Notifications exist!'
 // 	});
 // });
+
+mainContentsCallbacks.trigger();
