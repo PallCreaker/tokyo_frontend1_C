bc288687fd7795b095254d77b3e47b7215c76db6
Takahiro Watanabe <takahiro.watanabe1220@gmail.com>
2014-08-15 22:14:10 +0900

    パネルの背景色をランダムでカラフルに

--- vendor/assets/javascripts/myapps/mainpage-app.js
+++ vendor/assets/javascripts/myapps/mainpage-app.js
@@ -24,8 +24,15 @@ var mainContentsCallbacks = myApp.onPageInit('main', function(page) {
     });
     $(function() {
         myApp.popup('.popup-notification');
+        var flatcolors = [
+            '#1abc9c','#3498db','#9b59b6','#34495e',
+            '#16a085','#27ae60','#2980b9','#2c3e50',
+            '#f1c40f','#e67e22','#e74c3c','#95a5a6',
+            '#f39c12','#d35400','#c0392b','#7f8c8d'
+        ];
         $(".answerPanels").each(function() {
-
+            var rnd = Math.floor( Math.random() * flatcolors.length);
+            $(this).css('background-color',flatcolors[rnd]);
         });
     });
 });
