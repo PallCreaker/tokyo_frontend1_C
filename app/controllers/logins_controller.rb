a55a3a4d14c647a1c936fd0a0bd0878464613d6b
Matsuda Jumpei <j.matsuda.bb26th@gmail.com>
2014-08-16 17:45:27 +0900

    アラート表示とカテゴリのレンダリング #1 #4

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -3,6 +3,8 @@ class LoginsController < ApplicationController
   end
 
   def next
+    parent_cate_id = params[:category_id]
+    @categories = Category.get_children parent_cate_id
   end
 
   def last
