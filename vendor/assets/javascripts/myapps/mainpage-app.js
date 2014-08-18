967ad90295304d5a57340217eb5c4188e264982c
kazumasa Fujita <pallcreaker@gmail.com>
2014-08-18 13:14:01 +0900

    fixed indent tab into space4

--- vendor/assets/javascripts/myapps/mainpage-app.js
+++ vendor/assets/javascripts/myapps/mainpage-app.js
@@ -26,39 +26,39 @@ var mainContentsCallbacks = myApp.onPageInit('main', function(page) {
         }     
     });
 
-	$(function() {
+    $(function() {
         myApp.popup('.popup-notification');
         $(".footerNotification").slideDown();
     });
 
     var urlArray = location.href.split('/');
     $.getJSON('/ctc/matching/json/'+urlArray[urlArray.length-1], function(json) {
-    	var jsonLength = json.length;
-    	for (var i = 0; i < jsonLength; i++) {
-    		var specialsLength = json[i].specials.length;
-    		for (var j = 0; j < specialsLength; j++) {
-    			var howLength = json[i].specials[j].how_to_start.length;
-    			for (var k = 0; k < howLength; k++) {
-    				var howToStart = json[i].specials[j].how_to_start[k];
-    				var content = '最初:'+
-    					howToStart.first_content+
-    					'<br>'+
-    					'次:'+
-    					howToStart.next_content;
-    				var panelHtml = '<div class="answerPanels">'+content+'</div>';
-    				$('.page-content').append(panelHtml);
-    			}
-    		}
-    	};
-
-    	$$(".answerPanels").on('click', function() {
-                if(!$(this).hasClass("no-blur")){
-                    myApp.popup('.popup-about');
-                    thisPanel = $(this);
+        var jsonLength = json.length;
+        for (var i = 0; i < jsonLength; i++) {
+            var specialsLength = json[i].specials.length;
+            for (var j = 0; j < specialsLength; j++) {
+                var howLength = json[i].specials[j].how_to_start.length;
+                for (var k = 0; k < howLength; k++) {
+                    var howToStart = json[i].specials[j].how_to_start[k];
+                    var content = '最初:'+
+                        howToStart.first_content+
+                        '<br>'+
+                        '次:'+
+                        howToStart.next_content;
+                    var panelHtml = '<div class="answerPanels">'+content+'</div>';
+                    $('.page-content').append(panelHtml);
                 }
-    	});
+            }
+        };
 
-    	var flatcolors = [
+        $$(".answerPanels").on('click', function() {
+            if(!$(this).hasClass("no-blur")){
+                myApp.popup('.popup-about');
+                thisPanel = $(this);
+            }
+        });
+
+        var flatcolors = [
             '#1abc9c','#3498db','#9b59b6','#34495e',
             '#16a085','#27ae60','#2980b9','#2c3e50',
             '#f1c40f','#e67e22','#e74c3c','#95a5a6',
@@ -73,54 +73,51 @@ var mainContentsCallbacks = myApp.onPageInit('main', function(page) {
         var panelsLength = $answerPanels.length;
 
         for (var i = 0; i < panelsLength; i++) {
-        	// 基準パネルの設定
-	        var $answerPanel = $($answerPanels[i++]);
-	        var $answerPanelsSubset = $answerPanel;
+            // 基準パネルの設定
+            var $answerPanel = $($answerPanels[i++]);
+            var $answerPanelsSubset = $answerPanel;
 
-        	var wScale = (i == panelsLength)?wDividers[wDividers.length-2]:wDividers[Math.floor(Math.random() * (wDividers.length -1))];
-        	var hScale = hDividers[Math.floor(Math.random() * hDividers.length)];
+            var wScale = (i == panelsLength)?wDividers[wDividers.length-2]:wDividers[Math.floor(Math.random() * (wDividers.length -1))];
+            var hScale = hDividers[Math.floor(Math.random() * hDividers.length)];
 
-        	if (wScale * hScale > 8) {
-        		wScale--;
-        		hScale--;
-        	}
+            if (wScale * hScale > 8) {
+                wScale--;
+                hScale--;
+            }
 
-        	var pWidth = basePanelLength * wScale; //最後の要素ならfillさせる
-	        var pHeight = basePanelLength * hScale;
-	        var maxWidthScale = wDividers.length - wScale;
-	        var baseMaxHeightScale = hScale;
+            var pWidth = basePanelLength * wScale; //最後の要素ならfillさせる
+            var pHeight = basePanelLength * hScale;
+            var maxWidthScale = wDividers.length - wScale;
+            var baseMaxHeightScale = hScale;
 
-    		$answerPanel.css('background-color', flatcolors[Math.floor(Math.random() * flatcolors.length)]);
-			$answerPanel.width(pWidth);
-			$answerPanel.height(pHeight);
-			// 基準パネルの設定終わり．
-
-			while(maxWidthScale > 0) {
-				// 次のcolumnを作成する
-				var $nextColumn = $();
-				var maxHeightScale = baseMaxHeightScale;
-		        // column幅設定
-			    wScale = (i == panelsLength)?wDividers[maxWidthScale - 1]:wDividers[Math.floor(Math.random() * maxWidthScale)];
-			    pWidth = basePanelLength * wScale;
-		        maxWidthScale -= wScale;
-
-		        while(maxHeightScale > 0) {
-		        	hScale = hDividers[Math.floor(Math.random() * maxHeightScale)];
-			        pHeight = basePanelLength * hScale;
-			        maxHeightScale -= hScale;
-			        $answerPanel = $($answerPanels[i++]);
-		    	    $answerPanel.css('background-color', flatcolors[Math.floor(Math.random() * flatcolors.length)]);
-					$answerPanel.width(pWidth);
-					$answerPanel.height(pHeight);
-					$nextColumn = $nextColumn.add($answerPanel);
-				}
-				// column作成
-				$nextColumn.wrapAll('<div></div>');
-				$answerPanelsSubset = $answerPanelsSubset.add($nextColumn.parent());
-			}
-
-			// row作成
-			$answerPanelsSubset.wrapAll('<div class="row"></div>');
+            $answerPanel.css('background-color', flatcolors[Math.floor(Math.random() * flatcolors.length)]);
+            $answerPanel.width(pWidth);
+            $answerPanel.height(pHeight);
+            // 基準パネルの設定終わり
+            while(maxWidthScale > 0) {
+                // 次のcolumnを作成する
+                var $nextColumn = $();
+                var maxHeightScale = baseMaxHeightScale;
+                      // column幅設定
+                wScale = (i == panelsLength)?wDividers[maxWidthScale - 1]:wDividers[Math.floor(Math.random() * maxWidthScale)];
+                pWidth = basePanelLength * wScale;
+                maxWidthScale -= wScal;
+                while(maxHeightScale > 0) {
+                    hScale = hDividers[Math.floor(Math.random() * maxHeightScale)];
+                    pHeight = basePanelLength * hScale;
+                    maxHeightScale -= hScale;
+                    $answerPanel = $($answerPanels[i++]);
+                    $answerPanel.css('background-color', flatcolors[Math.floor(Math.random() * flatcolors.length)]);
+                    $answerPanel.width(pWidth);
+                    $answerPanel.height(pHeight);
+                    $nextColumn = $nextColumn.add($answerPanel);
+                }
+                // column作成
+                $nextColumn.wrapAll('<div></div>');
+                $answerPanelsSubset = $answerPanelsSubset.add($nextColumn.parent());
+            }
+            // row作成
+            $answerPanelsSubset.wrapAll('<div class="row"></div>');
         };
     });
 });
